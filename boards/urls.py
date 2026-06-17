aaa5b7ea2d84ad9478028d8371bff3e6d0d1b7b4
Milagros Alonso <milagrosalon9@gmail.com>
2026-06-17 20:28:55 -0300

    chore: eliminar archivos de estructura antigua tras refactorización

--- boards/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import BoardViewSet, ListViewSet, CardViewSet
-
-router = DefaultRouter()
-router.register(r'boards', BoardViewSet, basename='board')
-router.register(r'lists', ListViewSet, basename='list')
-router.register(r'cards', CardViewSet, basename='card')
-
-urlpatterns = router.urls
